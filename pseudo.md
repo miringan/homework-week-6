e691deeec6aad2c5b6788bc72d7e53748c13471e
Michael Iringan <miringan@uw.edu>
2021-12-14 16:50:23 -0800

    Added functions and global vars to script.js. Updated the input method for the text in the body of index.html.

--- pseudo.md
+++ pseudo.md
@@ -3,7 +3,7 @@ Print/Render the weather data to the page.
 From the <form> element, listen to the "submit"
 From the <button> element, listen to the "click"
 
-    Select <input>, get its value, and provide it to teh geo API
+    Select <input>, get its value, and provide it to the geo API
 
 From the <button> container element, listen to the <button> "click
 
@@ -11,7 +11,20 @@ From the <button> container element, listen to the <button> "click
 
 Fetch the geo data (lat, lon)
 
-function fetchGeoData (lat, lon) {
+function fetchGeoData (city) {
+
+    var url = "";
+
+    fetch(url) 
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
+
+            console.log(data);
+
+            fetchOneCall(lat, lon);
+        })
 
 }
 
@@ -22,6 +35,24 @@ function fetchGeoData (lat, lon) {
     appid = Your custom API key
 
 Fetch the one call weather data
+
+function fetchOneCall (lat, lon) {
+
+    var url = "";
+
+    fetch(url) 
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
+
+            console.log(data);
+
+            // Render to page function
+
+        })
+
+}
 
     lat
 
--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // Global Vars
+
+var searchButtonEl = $('#search-button');
 var cityNameSubmissionEl = $('#cityName');
-var searchSubmitEl = $
+// var searchSubmitEl = $
 
 // Event listener functions:
-cityNameSubmissionEl.on('submit',fetchGeoData(cityNameSubmissionEl.value));
+searchButtonEl.on('click',geoData);
 
 // Fetch data functions:
 
-function fetchGeoData (city) {
+function geoData (event) {
 
-    var url = "";
+    
+    // API Key: abd3df57a4ab15ebd0330fb0c538c405
+    var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityNameSubmissionEl.val() + "&limit=1&appid=abd3df57a4ab15ebd0330fb0c538c405";
+
+    //console.log(cityNameSubmissionEl.val());
 
     fetch(url) 
         .then(function(response){
@@ -19,15 +25,16 @@ function fetchGeoData (city) {
 
             console.log(data);
 
-            fetchOneCall(lat, lon);
+            fetchOneCall(data[0].lat, data[0].lon);
         })
 
 }
 
 function fetchOneCall (lat, lon) {
 
-    var url = "";
-
+    // API Key: abd3df57a4ab15ebd0330fb0c538c405
+    var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=abd3df57a4ab15ebd0330fb0c538c405";
+    
     fetch(url) 
         .then(function(response){
             return response.json();
@@ -44,3 +51,19 @@ function fetchOneCall (lat, lon) {
 
 // Print/Render functions:
 
+// Prints Today's Weather
+function renderCurrentWeather () {
+
+    // Print location, today's date, and an icon that shows the type of weather
+
+}
+
+// Prints the weather for the next few days
+function renderFiveDayForecast () {
+
+}
+
+// Adds the previous searches as buttons below the search button
+function previousSearchButtons () {
+
+}
